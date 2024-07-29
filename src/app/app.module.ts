f421a56da238ac3cd598e9fdf46b279d92dd6108
Ed <edwin.antonio.arroyo.lopez@gmail.com>
2024-07-28 19:32:22 -0500

    CM-006 add search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductService } from './product.service';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
-  ],
+    ProductListComponent,
+    SearchComponent
+    ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
+  exports: [SearchComponent],
   providers: [ProductService],
   bootstrap: [AppComponent]
 })
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../product.service';
 import { CommonModule } from '@angular/common';
+import { ProductService } from '../product.service';
+import { SearchComponent } from '../search/search.component';
 
 interface Product {
   id: string;
@@ -17,24 +18,33 @@ interface Product {
   styleUrls: ['./product-list.component.css'],
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    SearchComponent
   ],
 })
 export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
+  filteredProducts: Product[] = [];
 
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe(response => {
       this.products = response.data;
+      this.filteredProducts = response.data;
     });
   }
 
   getInitials(name: string): string {
     const initials = name.split(' ').map(word => word[0]).join('');
     return initials.slice(0, 2).toUpperCase();
+  }
+
+  onSearchTermChanged(searchTerm: string) {
+    this.filteredProducts = this.products.filter(product =>
+      product.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
   }
   
 }
